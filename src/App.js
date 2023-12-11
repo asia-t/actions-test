12899dbf3ef618bb76f4280ca5993566e06d3669
Joanna Derks <joanna.derks@schibsted.pl>
2023-12-11 12:13:39 +0100

    setting up

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.github.com/en/actions"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Github Actions
         </a>
       </header>
     </div>
